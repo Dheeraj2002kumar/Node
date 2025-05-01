4b37ce218ad6408a1a82f92bacdc8d1a6c98d6c2
Dheeraj Kumar <dk8549644@gmail.com>
2025-05-01 18:34:58 +0530

    Update file.js and test.txt to append timestamps to test.txt and organize comments

--- File_Handling_in_NodeJS/file.js
+++ File_Handling_in_NodeJS/file.js
@@ -2,10 +2,46 @@
 
 const fs = require("fs");
 
+// -------- Write file (Override) ----------------
 // Sync.. -> synchronous call
 // fs.writeFileSync("./text.txt", "Hello Node..");
 
 
 
 // Async.. -> Asynchronous call
-fs.writeFile("./test.txt", "Hello Node Async...", (err) => {});
+// fs.writeFile("./test.txt", "Hello Node Async...", (err) => {});
+
+
+// ------------- Read file ------------
+
+// Sync..
+// const result = fs.readFileSync("./contact.txt", "utf8");
+// console.log(result)
+
+
+// Async...
+// fs.readFile("./contact.txt", "utf8", (error, result) => {
+//     if (error){
+//         console.log("Error", error);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
+
+
+// -------------- Write file (Not override) -----------
+
+fs.appendFileSync("./test.txt", `\n ${Date.now()} Hey There.`)
+
+
+
+// ------------- Copy file -------------------
+
+// fs.cpSync("./test.txt", "./copy.txt")
+
+
+
+// ------------ Delete (unlink file) -----------
+
+fs.unlinkSync("./copy.txt")
