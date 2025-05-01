2d10ac3f62dba9c6fbf64660128f6dd51cb98029
Dheeraj Kumar <dk8549644@gmail.com>
2025-05-01 17:27:55 +0530

    Refactor index.js to use destructured imports for add and sub functions

--- Modules_in_NodeJS/index.js
+++ Modules_in_NodeJS/index.js
@@ -1,3 +1,9 @@
-const math = require("./math");
+// const math = require("./math");
 
-console.log("math value is ", math.add(2, 5));
+// console.log("math value is ", math.add(2, 5));
+
+
+const { add, sub } = require("./math");
+
+console.log("Math value is ", add(2, 5))
+console.log("Math value is ", sub(2, 5))
