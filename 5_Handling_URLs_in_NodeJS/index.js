777c404200a616950e38fed1f797b8cb19fcbb35
Dheeraj Kumar <dk8549644@gmail.com>
2025-05-03 21:37:15 +0530

    Enhance URL handling by adding query parameter support and updating response messages

--- 5_Handling_URLs_in_NodeJS/index.js
+++ 5_Handling_URLs_in_NodeJS/index.js
@@ -1,4 +1,42 @@
 
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+
+// const myServer = http.createServer((request, response) => {
+//     if (request.url === "/favicon.ico") {
+//         return response.end;
+//     }
+
+//     const log = `${Date.now()}: ${request.url} New Req Received\n`;
+//     const myUrl = url.parse(request.url);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt", log, (err, data) => {
+//         switch(request.url){
+//             case '/':
+//                 response.end("Home Page");
+//                 break;
+//             case '/about':
+//                 response.end("I am Dheeraj Kumar");
+//                 break;
+//             default:
+//                 response.end("404 Not Found")
+//         }
+//     })
+// });
+
+// myServer.listen(8000, () => console.log("Server Started!"));
+
+
+
+
+
+
+
+
+
+
+
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
@@ -9,15 +47,21 @@ const myServer = http.createServer((request, response) => {
     }
 
     const log = `${Date.now()}: ${request.url} New Req Received\n`;
-    const myUrl = url.parse(request.url);
+    const myUrl = url.parse(request.url, true);
     console.log(myUrl);
+
     fs.appendFile("log.txt", log, (err, data) => {
-        switch(request.url){
+        switch(myUrl.pathname){
             case '/':
                 response.end("Home Page");
                 break;
             case '/about':
-                response.end("I am Dheeraj Kumar");
+                const userName = myUrl.query.myname;
+                response.end(`Hii, ${userName}`);
+                break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                response.end("Here are your result for " + search);
                 break;
             default:
                 response.end("404 Not Found")
@@ -26,3 +70,13 @@ const myServer = http.createServer((request, response) => {
 });
 
 myServer.listen(8000, () => console.log("Server Started!"));
+
+
+
+
+
+
+
+
+
+
