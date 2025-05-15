f9e8728517afd551fe996fabec0b27617788f003
Dheeraj Kumar <dk8549644@gmail.com>
2025-05-15 08:45:06 +0530

    Implement user retrieval route and add placeholder for edit and delete operations

--- 8_BuildingREST_APIs_using_node_express/Project-1/index.js
+++ 8_BuildingREST_APIs_using_node_express/Project-1/index.js
@@ -5,17 +5,38 @@ const app = express();
 const PORT = 8000;
 
 // Routes
-app.get("/api/users", (req, res) => {
-    return res.json(users);
-});
+// app.get("/api/users", (req, res) => {
+//     return res.json(users);
+// });
 
-app.get("/user", (req, res) => {
-    const html = `
-    <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `;
-    res.send(html);
+// app.get("/user", (req, res) => {
+//     const html = `
+//     <ul>
+//         ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+//     </ul>
+//     `;
+//     res.send(html);
+// });
+
+// app.get("/api/users/:id", (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// })
+
+// create route
+
+app.route("/api/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+})
+.put((req, res) => {
+    // Edit user with id
+    res.json({ status: "Pending" });
+})
+.delete((req, res) => {
+
 })
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
